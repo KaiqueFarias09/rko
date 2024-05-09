1baf975f8cb4a313725b6ef803e75e00252c72d4
Kaique Farias <kaiquesouzafarias@outlook.com>
2024-05-09 10:25:51 -0300

    feat: :sparkles: Add route generator to MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:rko/core/colors/app_colors.dart';
+import 'package:rko/core/constants/image_paths.dart';
+import 'package:rko/presentation/routes/route_generator.dart';
+import 'package:rko/presentation/views/secure-the-area/secure_the_are_view.dart';
 import 'package:rko/presentation/widgets/app_appbar.dart';
 import 'package:rko/presentation/widgets/app_drawer.dart';
 import 'package:rko/presentation/widgets/button/app_primary_button.dart';
@@ -16,11 +20,21 @@ class RkoApp extends StatelessWidget {
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: AppColors.white,
+        statusBarIconBrightness: Brightness.light,
+        systemNavigationBarIconBrightness: Brightness.light,
+        systemNavigationBarColor: AppColors.white,
+      ),
+    );
+
     return MaterialApp(
       title: 'RKO',
       theme: ThemeData(useMaterial3: true),
       debugShowCheckedModeBanner: false,
       home: const Onboarding(),
+      onGenerateRoute: RouteGenerator().generateRoute,
     );
   }
 }
@@ -32,10 +46,7 @@ class Onboarding extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: const AppDrawer(),
-      appBar: const AppAppbar(
-        title: 'RKO',
-        shouldShowDrawer: true,
-      ),
+      appBar: const AppAppbar(title: 'RKO', shouldShowDrawer: true),
       body: Padding(
         padding: const EdgeInsets.fromLTRB(23, 34, 23, 30),
         child: Center(
@@ -43,8 +54,13 @@ class Onboarding extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               const SizedBox(),
-              SvgPicture.asset('assets/images/initial.svg'),
-              AppPrimaryButton(text: 'Start', onTap: () {}),
+              SvgPicture.asset(AppImages.initial),
+              AppPrimaryButton(
+                text: 'Start',
+                onTap: () => Navigator.of(context).pushNamed(
+                  SecureTheAreaView.id,
+                ),
+              ),
             ],
           ),
         ),
---
+++ lib/presentation/routes/route_generator.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:rko/presentation/views/home/home_view.dart';
+import 'package:rko/presentation/views/secure-the-area/secure_the_are_view.dart';
+
+class RouteGenerator {
+  Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case SecureTheAreaView.id:
+        return MaterialPageRoute(
+          builder: (builder) => const SecureTheAreaView(),
+        );
+
+      case HomeView.id:
+        return MaterialPageRoute(builder: (builder) => const HomeView());
+
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Error'),
+        ),
+        body: const Center(
+          child: Text('Error while loading new page'),
+        ),
+      );
+    });
+  }
+}
