95bc3957b5b1a0f8bcd656c74424aa4453910d83
Kaique Farias <kaiquesouzafarias@outlook.com>
2024-05-09 10:32:16 -0300

    refactor: :recycle: Remove OnboardingView from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:rko/core/colors/app_colors.dart';
-import 'package:rko/core/constants/image_paths.dart';
 import 'package:rko/presentation/routes/route_generator.dart';
-import 'package:rko/presentation/views/secure-the-area/secure_the_area_view.dart';
-import 'package:rko/presentation/widgets/app_appbar.dart';
-import 'package:rko/presentation/widgets/app_drawer.dart';
-import 'package:rko/presentation/widgets/button/app_primary_button.dart';
+import 'package:rko/presentation/views/onboarding/onboarding_view.dart';
 
 void main() {
   runApp(const RkoApp());
@@ -18,8 +13,6 @@ class RkoApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-
     SystemChrome.setSystemUIOverlayStyle(
       const SystemUiOverlayStyle(
         statusBarColor: AppColors.white,
@@ -29,42 +22,14 @@ class RkoApp extends StatelessWidget {
       ),
     );
 
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
     return MaterialApp(
       title: 'RKO',
       theme: ThemeData(useMaterial3: true),
       debugShowCheckedModeBanner: false,
-      home: const Onboarding(),
+      home: const OnboardingView(),
       onGenerateRoute: RouteGenerator().generateRoute,
-    );
-  }
-}
-
-class Onboarding extends StatelessWidget {
-  const Onboarding({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: const AppDrawer(),
-      appBar: const AppAppbar(title: 'RKO', shouldShowDrawer: true),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(23, 34, 23, 30),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const SizedBox(),
-              SvgPicture.asset(AppImages.initial),
-              AppPrimaryButton(
-                text: 'Start',
-                onTap: () => Navigator.of(context).pushNamed(
-                  SecureTheAreaView.id,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
---
+++ lib/presentation/views/onboarding/onboarding_view.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:rko/core/constants/image_paths.dart';
+import 'package:rko/presentation/views/secure-the-area/secure_the_area_view.dart';
+import 'package:rko/presentation/widgets/app_appbar.dart';
+import 'package:rko/presentation/widgets/app_drawer.dart';
+import 'package:rko/presentation/widgets/button/app_primary_button.dart';
+
+class OnboardingView extends StatelessWidget {
+  const OnboardingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: const AppDrawer(),
+      appBar: const AppAppbar(title: 'RKO', shouldShowDrawer: true),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(23, 34, 23, 30),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const SizedBox(),
+              SvgPicture.asset(AppImages.initial),
+              AppPrimaryButton(
+                text: 'Start',
+                onTap: () => Navigator.of(context).pushNamed(
+                  SecureTheAreaView.id,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
