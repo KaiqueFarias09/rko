436afe29415b1367c2c0ea564a5d63fbde986bd6
Kaique Farias <kaiquesouzafarias@outlook.com>
2024-05-10 10:19:20 -0300

    feat: :sparkles: Add audio functionality to SecureTheAreaView

--- lib/presentation/routes/route_generator.dart
+++ lib/presentation/routes/route_generator.dart
@@ -1,21 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rko/presentation/bloc/cubit/audio_cubit.dart';
 import 'package:rko/presentation/views/home/home_view.dart';
+import 'package:rko/presentation/views/onboarding/onboarding_view.dart';
 import 'package:rko/presentation/views/secure-the-area/secure_the_area_view.dart';
 
 class RouteGenerator {
   Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case SecureTheAreaView.id:
-        return MaterialPageRoute(
-          builder: (builder) => const SecureTheAreaView(),
-        );
+        return _buildAudioRoute(const SecureTheAreaView());
 
       case HomeView.id:
         return MaterialPageRoute(builder: (builder) => const HomeView());
 
+      case OnboardingView.id:
+        return MaterialPageRoute(builder: (builder) => const OnboardingView());
+
       default:
         return _errorRoute();
     }
+  }
+
+  Route<dynamic> _buildAudioRoute(Widget screen) {
+    return MaterialPageRoute(
+      builder: (_) => BlocProvider.value(
+        value: AudioCubit(),
+        child: screen,
+      ),
+    );
   }
 
   static Route<dynamic> _errorRoute() {
--- lib/presentation/views/secure-the-area/secure_the_area_view.dart
+++ lib/presentation/views/secure-the-area/secure_the_area_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:rko/core/colors/app_colors.dart';
 import 'package:rko/core/constants/image_paths.dart';
+import 'package:rko/presentation/bloc/cubit/audio_cubit.dart';
 import 'package:rko/presentation/views/home/home_view.dart';
 import 'package:rko/presentation/widgets/app_appbar.dart';
 import 'package:rko/presentation/widgets/bulleted_text.dart';
@@ -14,12 +16,12 @@ class SecureTheAreaView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final audioCubit = BlocProvider.of<AudioCubit>(context);
+    WidgetsBinding.instance.addPostFrameCallback((_) => audioCubit.play(id));
+
     return Scaffold(
       backgroundColor: AppColors.white,
-      appBar: const AppAppbar(
-        title: 'Bezpieczeństwo',
-        shouldShowAction: true,
-      ),
+      appBar: const AppAppbar(title: 'Bezpieczeństwo', shouldShowAction: true),
       body: Padding(
         padding: const EdgeInsets.fromLTRB(23, 34, 23, 30),
         child: Column(
@@ -38,7 +40,10 @@ class SecureTheAreaView extends StatelessWidget {
             ),
             AppPrimaryButton(
               text: 'Kontynuuj',
-              onTap: () => Navigator.of(context).pushNamed(HomeView.id),
+              onTap: () {
+                audioCubit.stop();
+                Navigator.of(context).pushReplacementNamed(HomeView.id);
+              },
             )
           ],
         ),
