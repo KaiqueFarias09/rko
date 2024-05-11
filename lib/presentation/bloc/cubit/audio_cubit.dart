1fadce46f2e1513d8eb3674f472eeb6e82d0ded2
Kaique Farias <kaiquesouzafarias@outlook.com>
2024-05-11 15:23:40 -0300

    feat: :sparkles: Create AudioViewScaffold for views that require audio functionality

--- lib/presentation/bloc/cubit/audio_cubit.dart
+++ lib/presentation/bloc/cubit/audio_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:just_audio/just_audio.dart';
@@ -12,8 +13,19 @@ class AudioCubit extends Cubit<AudioState> {
 
   final AudioPlayer _player;
   final assetsPath = 'assets/audios';
+  late String previousFile;
+
+  Future<void> navigateWithAudioTransition(
+    BuildContext context,
+    String routeName,
+  ) async {
+    await stop();
+    await Navigator.of(context).pushNamed(routeName);
+    await play(previousFile);
+  }
 
   Future<void> play(String file) async {
+    previousFile = file;
     await _player.setVolume(1);
     await _player.setAsset('$assetsPath/$file.mp3');
     await _player.play();
---
+++ lib/presentation/widgets/audio_view_scaffold.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rko/core/colors/app_colors.dart';
+import 'package:rko/core/constants/app_padding.dart';
+import 'package:rko/presentation/bloc/cubit/audio_cubit.dart';
+import 'package:rko/presentation/widgets/app_appbar.dart';
+import 'package:rko/presentation/widgets/app_bottom_navigation_bar.dart';
+
+class AudioViewScaffold extends StatelessWidget {
+  const AudioViewScaffold({
+    required this.id,
+    required this.body,
+    this.title,
+    this.bottomNavigationBar,
+    this.showSecondary = true,
+    this.nextScreen,
+    super.key,
+  }) : assert(
+          bottomNavigationBar != null || nextScreen != null,
+          'nextScreen cannot be empty when bottomNavigationBar is null.',
+        );
+
+  final String id;
+  final Widget body;
+  final bool showSecondary;
+  final String? nextScreen;
+  final String? title;
+  final Widget? bottomNavigationBar;
+
+  @override
+  Widget build(BuildContext context) {
+    final audioCubit = BlocProvider.of<AudioCubit>(context);
+    WidgetsBinding.instance.addPostFrameCallback((_) => audioCubit.play(id));
+
+    return PopScope(
+      onPopInvoked: (didPop) async {
+        if (!didPop) return;
+        await audioCubit.stop();
+      },
+      child: Scaffold(
+        backgroundColor: AppColors.white,
+        appBar: AppAppbar(
+          title: title ?? '',
+          shouldShowAction: true,
+          actionOnTap: () async {
+            await audioCubit.stop();
+          },
+        ),
+        bottomNavigationBar: bottomNavigationBar ??
+            AppBottomNavigationBar(
+              primaryOnTap: () async {
+                await audioCubit.navigateWithAudioTransition(
+                  context,
+                  nextScreen!,
+                );
+              },
+              showSecondary: showSecondary,
+            ),
+        body: Padding(
+          padding: AppPaddings.defaultPadding,
+          child: body,
+        ),
+      ),
+    );
+  }
+}
