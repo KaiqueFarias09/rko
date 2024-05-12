2f62ea01b432a587d678b0e36e7966fc84b93aef
Kaique Farias <kaiquesouzafarias@outlook.com>
2024-05-12 18:17:18 -0300

    feat: :sparkles: Make cpr button play cpr-animation audio instead of navigating to CprView

--- lib/presentation/views/heart-massage-begins/heart_massage_begins_view.dart
+++ lib/presentation/views/heart-massage-begins/heart_massage_begins_view.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:rko/core/constants/app_images.dart';
 import 'package:rko/core/icons/app_icons.dart';
-import 'package:rko/presentation/views/cpr/cpr_view.dart';
+import 'package:rko/presentation/bloc/audio/audio_cubit.dart';
 import 'package:rko/presentation/views/mouth-to-mouth-1/mouth_to_mouth_1_view.dart';
+import 'package:rko/presentation/widgets/app_bottom_navigation_bar.dart';
 import 'package:rko/presentation/widgets/audio_view_scaffold.dart';
 import 'package:rko/presentation/widgets/bulleted_text.dart';
 import 'package:rko/presentation/widgets/button/app_secondary_button.dart';
@@ -15,35 +17,54 @@ class HeartMassageBeginsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AudioViewScaffold(
-      id: id,
-      nextScreen: MouthToMouth1View.id,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          SvgPicture.asset(
-            AppImages.heartMassageBegins,
-            height: 300,
+    final audioCubit = BlocProvider.of<AudioCubit>(context);
+
+    return BlocBuilder<AudioCubit, AudioState>(
+      builder: (context, state) {
+        final status = state.status;
+        final currentTrack = state.currentTrack;
+
+        return AudioViewScaffold(
+          id: id,
+          bottomNavigationBar: AppBottomNavigationBar(
+            primaryOnTap: () async {
+              await audioCubit.stop();
+              await Navigator.of(context).pushNamed(MouthToMouth1View.id);
+              await audioCubit.play(id);
+            },
           ),
-          const Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              BulletedText('Rozpocznij ucisk klatki piersiowej.'),
-              BulletedText(
-                'Ułóż swoją dłoń na klatce piersiowej i spleć palce z drugą. Na wyprostowanych ramionach uciskaj klatkę do głębokości 5 cm. Nie odrywając dłoni zwolnij ucisk. Uciskaj 100 - 120 razy na minutę.',
+              SvgPicture.asset(
+                AppImages.heartMassageBegins,
+                height: 300,
+              ),
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BulletedText('Rozpocznij ucisk klatki piersiowej.'),
+                  BulletedText(
+                    'Ułóż swoją dłoń na klatce piersiowej i spleć palce z drugą. Na wyprostowanych ramionach uciskaj klatkę do głębokości 5 cm. Nie odrywając dłoni zwolnij ucisk. Uciskaj 100 - 120 razy na minutę.',
+                  ),
+                ],
+              ),
+              AppSecondaryButton(
+                text: 'Rytm do uciskania serca',
+                icon: AppIcons.heart,
+                onTap: () async {
+                  if (status == AudioStatus.stopped || currentTrack == id) {
+                    await audioCubit.play('cpr-animation');
+                    return;
+                  }
+
+                  await audioCubit.stop();
+                },
               ),
             ],
           ),
-          AppSecondaryButton(
-            text: 'Tempo uciskania',
-            icon: AppIcons.heart,
-            onTap: () => Navigator.of(context).pushNamedAndRemoveUntil(
-              CprView.id,
-              (route) => false,
-            ),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
