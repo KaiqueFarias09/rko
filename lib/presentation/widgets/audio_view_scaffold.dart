3299fb58b4106402ee4b3f11adc9b180c5b3f9b3
Kaique Farias <kaiquesouzafarias@outlook.com>
2024-05-12 18:15:51 -0300

    feat: :sparkles: Make audio views aware of app lifecycle changes

--- lib/presentation/widgets/audio_view_scaffold.dart
+++ lib/presentation/widgets/audio_view_scaffold.dart
@@ -6,7 +6,7 @@ import 'package:rko/presentation/bloc/audio/audio_cubit.dart';
 import 'package:rko/presentation/widgets/app_appbar.dart';
 import 'package:rko/presentation/widgets/app_bottom_navigation_bar.dart';
 
-class AudioViewScaffold extends StatelessWidget {
+class AudioViewScaffold extends StatefulWidget {
   const AudioViewScaffold({
     required this.id,
     required this.body,
@@ -28,10 +28,35 @@ class AudioViewScaffold extends StatelessWidget {
   final Widget? bottomNavigationBar;
 
   @override
-  Widget build(BuildContext context) {
-    final audioCubit = BlocProvider.of<AudioCubit>(context);
-    WidgetsBinding.instance.addPostFrameCallback((_) => audioCubit.play(id));
+  State<AudioViewScaffold> createState() => _AudioViewScaffoldState();
+}
 
+class _AudioViewScaffoldState extends State<AudioViewScaffold>
+    with WidgetsBindingObserver {
+  late final AudioCubit audioCubit;
+
+  @override
+  void initState() {
+    super.initState();
+    audioCubit = BlocProvider.of<AudioCubit>(context);
+    audioCubit.play(widget.id);
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    if (state == AppLifecycleState.paused) audioCubit.stop();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return PopScope(
       onPopInvoked: (didPop) async {
         if (!didPop) return;
@@ -40,25 +65,25 @@ class AudioViewScaffold extends StatelessWidget {
       child: Scaffold(
         backgroundColor: AppColors.white,
         appBar: AppAppbar(
-          title: title ?? '',
+          title: widget.title ?? '',
           shouldShowAction: true,
           actionOnTap: () async {
             await audioCubit.stop();
           },
         ),
-        bottomNavigationBar: bottomNavigationBar ??
+        bottomNavigationBar: widget.bottomNavigationBar ??
             AppBottomNavigationBar(
               primaryOnTap: () async {
                 await audioCubit.navigateWithAudioTransition(
                   context,
-                  nextScreen!,
+                  widget.nextScreen!,
                 );
               },
-              showSecondary: showSecondary,
+              showSecondary: widget.showSecondary,
             ),
         body: Padding(
           padding: AppPaddings.defaultPadding,
-          child: body,
+          child: widget.body,
         ),
       ),
     );
