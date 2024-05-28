ce4da5e0fc56b6a83b7f34dba1c6c87ae24120ce
Kaique Farias <kaiquesouzafarias@outlook.com>
2024-05-28 14:38:54 -0300

    feat: :sparkles: update app icons

--- lib/core/icons/app_icons.dart
+++ lib/core/icons/app_icons.dart
@@ -2,19 +2,67 @@ import 'package:flutter/widgets.dart';
 
 class AppIcons {
   AppIcons._();
+
   static const _kFontFam = 'RkoIcons';
-  static const IconData sound = IconData(0xe800, fontFamily: _kFontFam);
-  static const IconData chevronLeft = IconData(0xe801, fontFamily: _kFontFam);
-  static const IconData x = IconData(0xe802, fontFamily: _kFontFam);
-  static const IconData menu = IconData(0xe803, fontFamily: _kFontFam);
-  static const IconData chevronRight = IconData(0xe804, fontFamily: _kFontFam);
-  static const IconData call = IconData(0xe806, fontFamily: _kFontFam);
-  static const IconData notBreathing = IconData(0xe807, fontFamily: _kFontFam);
-  static const IconData stop = IconData(0xe808, fontFamily: _kFontFam);
-  static const IconData geo = IconData(0xe809, fontFamily: _kFontFam);
-  static const IconData caretDown = IconData(0xe80a, fontFamily: _kFontFam);
-  static const IconData play = IconData(0xe80b, fontFamily: _kFontFam);
-  static const IconData breathing = IconData(0xe811, fontFamily: _kFontFam);
-  static const IconData heart = IconData(0xe812, fontFamily: _kFontFam);
-  static const IconData goBack = IconData(0xe813, fontFamily: _kFontFam);
+
+  static const IconData sound = IconData(
+    0xe800,
+    fontFamily: _kFontFam,
+  );
+  static const IconData chevronLeft = IconData(
+    0xe801,
+    fontFamily: _kFontFam,
+  );
+  static const IconData x = IconData(
+    0xe802,
+    fontFamily: _kFontFam,
+  );
+  static const IconData menu = IconData(
+    0xe803,
+    fontFamily: _kFontFam,
+  );
+  static const IconData chevronRight = IconData(
+    0xe804,
+    fontFamily: _kFontFam,
+  );
+  static const IconData heart = IconData(
+    0xe805,
+    fontFamily: _kFontFam,
+  );
+  static const IconData call = IconData(
+    0xe806,
+    fontFamily: _kFontFam,
+  );
+  static const IconData notBreathing = IconData(
+    0xe807,
+    fontFamily: _kFontFam,
+  );
+  static const IconData stop = IconData(
+    0xe808,
+    fontFamily: _kFontFam,
+  );
+  static const IconData geo = IconData(
+    0xe809,
+    fontFamily: _kFontFam,
+  );
+  static const IconData caretDown = IconData(
+    0xe80a,
+    fontFamily: _kFontFam,
+  );
+  static const IconData play = IconData(
+    0xe80b,
+    fontFamily: _kFontFam,
+  );
+  static const IconData breathing = IconData(
+    0xe811,
+    fontFamily: _kFontFam,
+  );
+  static const IconData muted = IconData(
+    0xe812,
+    fontFamily: _kFontFam,
+  );
+  static const IconData goBack = IconData(
+    0xe813,
+    fontFamily: _kFontFam,
+  );
 }
